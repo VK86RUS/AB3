8976b6b69619b073992abeaa9623d79828191e18
BUBA <buba86@ro.ru>
2023-09-27 11:53:59 +0500

    True build Test1

--- src/test/java/ru/netology/CardSiteTest.java
+++ src/test/java/ru/netology/CardSiteTest.java
@@ -1,15 +1,18 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class CardSiteTest {
-    private static WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     public static void setupAll() {
@@ -17,46 +20,66 @@ public class CardSiteTest {
     }
 
     @BeforeEach
-    public void beforeEach() {
+    public void BeforeEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999/");
+        driver.get("http://localhost:9999");
     }
 
-    @AfterEach
-    public void afterEach() {
+    public void AfterEach() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    public void trueForm() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван Иванович");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79824133177");
+    void shouldSuccessfullyValidateFormWithCorrectValues() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79876543210");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
     }
 
     @Test
-    void falseFormeName() {
+    void shouldDetectInvalidNameAndShowErrorMessage() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Ivan");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79876543210");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
                 driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText());
+
     }
 
     @Test
-    void nullFormePhone() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван Иванович");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+    void shouldBeFailedForEmptyFieldName() {
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79876543210");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        Assertions.assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText());
+
+    }
+
+    @Test
+    void shouldBeFailedForIncorrectNumberField() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("89876543210");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText());
+
+    }
+
+    @Test
+    void shouldBeFailedForEmptyNumberField() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов иван");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         Assertions.assertEquals("Поле обязательно для заполнения",
@@ -64,12 +87,12 @@ public class CardSiteTest {
     }
 
     @Test
-    void falseFormePhone() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван Иванович");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+798241331777");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+    public void shouldBeFailedForEmptyNumberFieldAgreementCheckbox() {
+        // Ваш код для заполнения полей имени и номера телефона
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79876543210");
         driver.findElement(By.cssSelector("button.button")).click();
-        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
-                driver.findElement((By.cssSelector("[data-test-id=phone].input_invalid .input__sub"))).getText());
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
+
     }
 }
